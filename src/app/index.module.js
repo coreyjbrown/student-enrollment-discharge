4bfda6d1b3b18ba3e17f02fe4d2395a97175a8b5
coreyjbrown <coreyjbrown@gmail.com>
2016-03-30 11:21:11 -0400

    Run generator to add bootstrap

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('studentEnrollmentDischarge', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
